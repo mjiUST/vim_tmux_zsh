fcc146b4064d62ccb78a1a59c607297d41c77e04
mjiUST <mji@connect.ust.hk>
2018-05-12 23:28:23 +0200

    edit bashrc, clear the lines about PS1

--- installWorkingEnv.sh
+++ installWorkingEnv.sh
@@ -1,24 +1,10 @@
+#!/bin/bash -vx
 
-set -e # To exit the script as soon as one of the commands failed
-# requirements: vim, tmux
-if which vim tmux zsh | grep "not" > /dev/null; then echo "need to install vim tmux zsh"; sudo apt-get install vim tmux zsh; bash ~/.bashrc; fi
-
-#######
-# tmux
-#######
-if [ -f ~/.tmux.conf ]; then cp ~/.tmux.conf ~/.backup_tmux.conf; echo ".backup_tmux.conf was stored"; fi
-cp config/tmux.conf ~/.tmux.conf
-echo "NOTE that the new .tmux.conf changed the tmux bind key to <alt>m !!!"
-
-
-#######
-# oh-my-zsh
-#######
-if [ -d ~/.oh-my-zsh ]; then cp -r ~/.oh-my-zsh ~/.backup_oh-my-zsh; rm -rf ~/.oh-my-zsh; echo ".backup_oh-my-zsh was stored"; fi
-bash config/install_oh_my_zsh.sh
-if [ -f ~/.zshrc ]; then cp ~/.zshrc ~/.backup_zshrc; echo ".backup_zshrc was stored"; fi
-cp config/zshrc ~/.zshrc
-cp config/kphoen.zsh-theme ~/.oh-my-zsh/themes/
+# requirements: vim, tmux, zsh
+# before run this script, install pkgs:
+    # sudo apt-get install vim tmux zsh
+# after run this script, set zsh as default:
+    # sudo chsh --shell=/bin/zsh mengqi
 
 
 #######
@@ -32,4 +18,23 @@ cp config/vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 
+#######
+# tmux
+#######
+if [ -f ~/.tmux.conf ]; then cp ~/.tmux.conf ~/.backup_tmux.conf; echo ".backup_tmux.conf was stored"; fi
+cp config/tmux.conf ~/.tmux.conf
+echo "NOTE that the new .tmux.conf changed the tmux bind key to <alt>m !!!"
+tmux source-file ~/.tmux.conf
 
+
+#######
+# oh-my-zsh
+#######
+if [ -d ~/.oh-my-zsh ]; then cp -r ~/.oh-my-zsh ~/.backup_oh-my-zsh; rm -rf ~/.oh-my-zsh; echo ".backup_oh-my-zsh was stored"; fi
+bash config/install_oh_my_zsh.sh
+if [ -f ~/.zshrc ]; then cp ~/.zshrc ~/.backup_zshrc; echo ".backup_zshrc was stored"; fi
+cp config/zshrc ~/.zshrc
+cp config/kphoen.zsh-theme ~/.oh-my-zsh/themes/
+if [ -f ~/.bashrc ]; then cp ~/.bashrc ~/.backup_bashrc; echo ".backup_bashrc was stored"; fi
+sed -i 's/PS1/: #PS1/g' ~/.bashrc  # remove all the commands related to PS1
+zsh
